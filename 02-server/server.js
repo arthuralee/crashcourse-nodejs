d6566479c1d6dccebc894a3a926c286f7478062b
Arthur Lee <arthur@arthurlee.me>
2013-10-31 13:19:27 -0400

    Added simple HTTP server

---
+++ 02-server/server.js
@@ -0,0 +1,10 @@
+// include the HTTP node module
+var http = require("http");
+
+// create HTTP server that responds with Hello World
+var server = http.createServer(function (req, res) {
+	res.end("Hello World\n");
+});
+
+// Bind the server to listen on port 8080
+server.listen(8080);
